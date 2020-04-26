0486c403a1326b94c0f03c985d9afec06711ddb1
Ivan <Ivan7771998>
2020-04-26 09:52:18 +0300

    conflict resolved true

--- app/src/main/java/com/example/weatherappgeekbrains/MainActivity.java
+++ app/src/main/java/com/example/weatherappgeekbrains/MainActivity.java
@@ -20,95 +20,10 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String TAG = "MainActivity";
-    private final static String NAME_CITY = "name_city";
-
-    @BindView(R.id.showPrecipitation)
-    CheckBox showPrecipitation;
-    @BindView(R.id.showHumidity)
-    CheckBox showHumidity;
-    @BindView(R.id.showWind)
-    CheckBox showWind;
-    @BindView(R.id.textPrecipitation)
-    TextView textPrecipitation;
-    @BindView(R.id.textHumidity)
-    TextView textHumidity;
-    @BindView(R.id.textWind)
-    TextView textWind;
-    @BindView(R.id.severalText)
-    EditText severalText;
-    @BindView(R.id.textViewCheck)
-    TextView textViewCheck;
-    @BindView(R.id.setButtonText)
-    MaterialButton setButtonText;
-    @BindView(R.id.titleWeather)
-    TextView titleWeather;
-    @BindView(R.id.showInBrowserBtn)
-    MaterialButton showInBrowserBtn;
-
-    private MainPresenter presenter;
-
-    public static Intent newInstance(Context context, String nameCity) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.putExtra(NAME_CITY, nameCity);
-        return intent;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        presenter = MainPresenter.getInstance();
-        setTitleWeather();
-        initWorkViewCheckBox();
-        openWeatherInBrowser();
-    }
-
-    private void openWeatherInBrowser() {
-        showInBrowserBtn.setOnClickListener(v -> {
-            startActivity(new Intent(android.content.Intent.ACTION_VIEW,
-                    Uri.parse("https://yandex.ru/pogoda/moscow")));
-        });
-    }
-
-    private void setTitleWeather() {
-        if (Objects.requireNonNull(getIntent().getExtras()).containsKey(NAME_CITY)) {
-            presenter.setTitleWeather(getIntent().getStringExtra(NAME_CITY));
-            titleWeather.setText(presenter.getTitleWeather());
-        }
-    }
-
-    private void initWorkViewCheckBox() {
-        textViewCheck.setVisibility(presenter.getShowCheckView());
-        textViewCheck.setText(presenter.getTextCheckView());
-        showPrecipitation.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            checkVisibility(textPrecipitation, isChecked);
-        });
-
-        showHumidity.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            checkVisibility(textHumidity, isChecked);
-        });
-
-        showWind.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            checkVisibility(textWind, isChecked);
-        });
-
-        setButtonText.setOnClickListener(v -> {
-            if (!"".contentEquals(severalText.getText())) {
-                presenter.setShowCheckView(View.VISIBLE);
-                presenter.setTextCheckView(severalText.getText().toString());
-                textViewCheck.setVisibility(presenter.getShowCheckView());
-                textViewCheck.setText(presenter.getTextCheckView());
-            }
-        });
-    }
-
-    private void checkVisibility(TextView text, boolean isChecked) {
-        if (isChecked) {
-            text.setVisibility(View.VISIBLE);
-        } else {
-            text.setVisibility(View.GONE);
-        }
     }
 }
--- app/src/main/java/com/example/weatherappgeekbrains/SelectCityActivity.java
+++ app/src/main/java/com/example/weatherappgeekbrains/SelectCityActivity.java
@@ -13,23 +13,9 @@ import butterknife.ButterKnife;
 
 public class SelectCityActivity extends AppCompatActivity {
 
-    @BindView(R.id.materialButton)
-    MaterialButton materialButton;
-    @BindView(R.id.searchCityText)
-    TextInputEditText searchCityText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_city);
-        ButterKnife.bind(this);
-
-        materialButton.setOnClickListener(v -> {
-            if (!Objects.requireNonNull(searchCityText.getText()).toString().equals("")) {
-                startActivity(MainActivity.newInstance(this, searchCityText.getText().toString()));
-            } else {
-                Toast.makeText(this, getResources().getString(R.string.text_toast), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
